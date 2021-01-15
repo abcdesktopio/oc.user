8a5a058a48722972bdf8f559898621d51b349a28
kecsou <kevin.voyer.developpeur@gmail.com>
2021-01-15 11:06:22 +0100

    don't provide container's ip to run test

--- make-test.sh
+++ make-test.sh
@@ -2,7 +2,6 @@
 
 echo "Run oc.user..."
 CONTAINER_ID=$(docker run --rm --env TESTING_MODE='true' -d abcdesktopio/oc.user.18.04)
-CONTAINER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${CONTAINER_ID})
 
 echo "Container ID: ${CONTAINER_ID}"
 
@@ -10,7 +9,7 @@ echo "Waiting 30s for cupsd and puslaudio"
 sleep 30
 
 echo "Run tests..."
-docker exec ${CONTAINER_ID} bash -e /composer/node/run-tests.sh ${CONTAINER_IP}
+docker exec ${CONTAINER_ID} bash -e /composer/node/run-tests.sh
 
 echo "Stop container ${CONTAINER_ID}..."
 docker stop -t 0 ${CONTAINER_ID}
