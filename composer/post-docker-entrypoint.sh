aa951ecebc0b0ab87f2440875eef3fc265ba087e
Alexandre DEVELY <12896316+alexandredevely@users.noreply.github.com>
2021-05-28 12:11:55 +0200

    add lsb_release test for 18.04

--- composer/post-docker-entrypoint.sh
+++ composer/post-docker-entrypoint.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 DEFAULT_TIMEOUT=5
 COUNTER=0
+RELEASE=$(lsb_release --release | cut -f2)
 POSTPONE_SERVICE="cupsd pulseaudio"
+echo "RELEASE=$RELEASE"
+if [ $RELEASE == "18.04" ]; then
+        POSTPONE_SERVICE="xsettingsd cupsd pulseaudio"
+fi
 while [  $COUNTER -lt 5 ]; do
-        # sleep $DEFAULT_TIMEOUT
+        sleep $DEFAULT_TIMEOUT
         echo "Starting supervisor post pone service $COUNTER/10" >> /var/log/desktop/postpone.services.log
         COUNTER=$((COUNTER+1))
         /usr/bin/supervisorctl status >> /var/log/desktop/postpone.services.log
         result=$?
-	echo "status return $result" >> /var/log/desktop/postpone.services.log
+        echo "status return $result" >> /var/log/desktop/postpone.services.log
         if [ $result -eq 0 ]; then
-		echo "supervisor service is started" >> /var/log/desktop/postpone.services.log
+                echo "supervisor service is started" >> /var/log/desktop/postpone.services.log
                 for service in $POSTPONE_SERVICE; do
                         /usr/bin/supervisorctl start $service  >> /var/log/desktop/postpone.services.log
                 done
