47879b50ded54cb14992c7441d8375310f892afe
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2021-07-06 17:10:37 +0200

    add BIND_INTERFACE

--- composer/wsproxy.sh
+++ composer/wsproxy.sh
@@ -7,14 +7,23 @@ while [ ! -S $X11VNCSOCKET ]; do
 done
 
 
+BIND_INTERFACE=''
+if [ "$DISABLE_REMOTEIP_FILTERING" == "enabled" ]; then
+	BIND_INTERFACE="0.0.0.0:6081"
+else
+	BIND_INTERFACE="$(hostname -i):6081"
+fi
+
+echo "DISABLE_REMOTEIP_FILTERING is $DISABLE_REMOTEIP_FILTERING, listening $BIND_INTERFACE"
+
 if [ "$USE_CERTBOT_CERTONLY" == "enabled" ]; then
 	FQDN="$EXTERNAL_DESKTOP_HOSTNAME.$EXTERNAL_DESKTOP_DOMAIN"
 	# certificats files are located in /etc/letsencrypt/live/
 	CERT="/etc/letsencrypt/live/$FQDN/fullchain.pem"
 	PRIVKEY="/etc/letsencrypt/live/$FQDN/privkey.pem"
-	echo "/composer/wsproxy.py --key=$PRIVKEY --cert=$CERT --unix-target $X11VNCSOCKET 6081" > /var/var/deskop/wsproxy.log
-	/composer/wsproxy.py --key=$PRIVKEY --cert=$CERT --unix-target $X11VNCSOCKET 6081
+	echo "/composer/wsproxy.py --key=$PRIVKEY --cert=$CERT --unix-target $X11VNCSOCKET $BIND_INTERFACE6081" > /var/var/deskop/wsproxy.log
+	/composer/wsproxy.py --key=$PRIVKEY --cert=$CERT --unix-target $X11VNCSOCKET $BIND_INTERFACE
 else
-	/composer/wsproxy.py --unix-target $X11VNCSOCKET 6081
+	/composer/wsproxy.py --unix-target $X11VNCSOCKET $BIND_INTERFACE
 fi
 
