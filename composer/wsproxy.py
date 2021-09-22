cc6ea2016201ecdb3129659f5b9ae33cc204ce15
Alexandre DEVELY <12896316+alexandredevely@users.noreply.github.com>
2021-09-22 15:16:01 +0200

    change to websockify>=0.9.0

--- composer/wsproxy.py
+++ composer/wsproxy.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3.6
-# EASY-INSTALL-ENTRY-SCRIPT: 'websockify==0.9.0','console_scripts','websockify'
+# EASY-INSTALL-ENTRY-SCRIPT: 'websockify>=0.9.0','console_scripts','websockify'
 __requires__ = 'websockify==0.9.0'
 import re
 import sys
@@ -8,5 +8,5 @@ from pkg_resources import load_entry_point
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
     sys.exit(
-        load_entry_point('websockify==0.9.0', 'console_scripts', 'websockify')()
+        load_entry_point('websockify>=0.9.0', 'console_scripts', 'websockify')()
     )
