3beea6e72d4668b91b272df7387200143db58362
Alexandre DEVELY <12896316+alexandredevely@users.noreply.github.com>
2021-10-11 23:10:23 +0200

    Update run-tests.sh

--- composer/node/run-tests.sh
+++ composer/node/run-tests.sh
@@ -1,19 +1,10 @@
 #/bin/bash
 
 export DISPLAY=:0.0
-
-echo "test if yarn is installed"
-YARN_VERSION=$(yarn --version)
-YARN_COMMAND=$?
-if [ $YARN_COMMAND -eq 0 ]; then
-  echo "command yarn --version success $YARN_VERSION"
-else
-  echo "command yarn failed $YARN_COMMAND"
-	echo "install yarn using npm"
-	cd 
-	npm install yarn
-	export PATH=$PATH:/home/balloon/node_modules/yarn/bin/yarn
-fi
+echo "install yarn using npm"
+cd 
+npm install yarn
+export PATH=$PATH:/home/balloon/node_modules/yarn/bin
 
 CONTAINER_IP=$(hostname -I | awk '{print $1}')
 
