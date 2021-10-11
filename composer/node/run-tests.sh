f381e8d7b5a9cd9ac2848e0f0e00a7c5402eb49e
Alexandre DEVELY <12896316+alexandredevely@users.noreply.github.com>
2021-10-11 22:38:31 +0200

    add npm install yarn

--- composer/node/run-tests.sh
+++ composer/node/run-tests.sh
@@ -2,6 +2,19 @@
 
 export DISPLAY=:0.0
 
+echo "test if yarn is installed"
+YARN_VERSION=$(yarn --version)
+YARN_COMMAND=$?
+if [ $YARN_COMMAND -eq 0 ]; then
+  echo "command yarn --version success $YARN_VERSION"
+else
+  echo "command yarn failed $YARN_COMMAND"
+	echo "install yarn using npm"
+	cd 
+	npm install yarn
+	export PATH=$PATH:/home/balloon/node_modules/yarn/bin/yarn
+fi
+
 CONTAINER_IP=$(hostname -I | awk '{print $1}')
 
 cd /composer/node/spawner-service 
