3b2dab8ccf05e898c69518d8c2eed46252956e83
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2022-05-11 11:29:43 +0200

    add new install as root

---
+++ composer/node/install-tests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# yarn install set --production=true
+# this script install missing package without --production=true option
+npm install -g yarn
+apt-get update && apt-get install -y libxmu-dev
+
+echo "install /composer/node/spawner-service"
+cd /composer/node/spawner-service
+/composer/node/spawner-service/node_modules/yarn/bin/yarn install
+
+echo "install /composer/node/spawner-service"
+cd /composer/node/spawner-service
+yarn install
+
+echo "install /composer/node/file-service"
+cd /composer/node/file-service
+yarn install
+
+echo "install /composer/node/broadcast-service"
+cd /composer/node/broadcast-service
+yarn install
+
+echo "install /composer/node/xterm.js"
+cd /composer/node/xterm.js
+yarn install
+
+echo "full install yarn done"
+~                                                
