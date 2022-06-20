a87ef12263ef879f0c1f84db2243bfd25e3e329b
Alexandre DEVELY <12896316+alexandredevely@users.noreply.github.com>
2022-06-20 11:32:07 +0200

    Update install-tests.sh

--- composer/node/install-tests.sh
+++ composer/node/install-tests.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-# yarn install set --production=true
-# this script install missing package without --production=true option
+#!/bin/bash
+
+# The default install use yarn install
+# yarn install --production=true
+# this script install missing package with --production=false option
+# yarn install --production=false
 
 #curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
 #echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 #apt-get update && apt-get install yarn
 #apt-get install -y npm
 
+apt-get update
 apt-get install -y 	\
 	libxmu-dev	\
 	gcc             \
@@ -20,18 +25,18 @@ apt-get install -y 	\
 # install full options without production
 echo "install /composer/node/spawner-service"
 cd /composer/node/spawner-service
-yarn install
+yarn install --production=false
 
 echo "install /composer/node/file-service"
 cd /composer/node/file-service
-yarn install
+yarn install --production=false
 
 echo "install /composer/node/broadcast-service"
 cd /composer/node/broadcast-service
-yarn install
+yarn install --production=false
 
 echo "install /composer/node/xterm.js"
 cd /composer/node/xterm.js
-yarn install
+yarn install --production=false
 
 echo "full install yarn done"
