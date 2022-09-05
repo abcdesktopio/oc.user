2425ea5f0c0d7467c14534186171ad37ced186e7
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2022-09-05 13:55:03 +0200

    run test is TARGET_MODE

--- composer/node/run-tests.sh
+++ composer/node/run-tests.sh
@@ -7,11 +7,17 @@ CONTAINER_IP=$(hostname -I | awk '{print $1}')
 cd /composer/node/spawner-service 
 CONTAINER_IP=${CONTAINER_IP} yarn test
 
-cd /composer/node/file-service 
-CONTAINER_IP=${CONTAINER_IP} yarn test
+# file service exists only in docker mode
+if [ $(cat /TARGET_MODE) = docker ]; then 
+	cd /composer/node/file-service 
+	CONTAINER_IP=${CONTAINER_IP} yarn test
+fi
 
 cd /composer/node/broadcast-service 
 CONTAINER_IP=${CONTAINER_IP} yarn test
 
-cd /composer/node/xterm.js 
-CONTAINER_IP=${CONTAINER_IP} yarn test
+# hardening disable xterm.js 
+if [ $(cat /TARGET_MODE) != hardening ]; then 
+	cd /composer/node/xterm.js 
+	CONTAINER_IP=${CONTAINER_IP} yarn test
+fi
