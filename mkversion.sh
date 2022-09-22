94cc12e90dbe6ad2637345a38d462a68450872cd
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2022-09-22 13:33:55 +0200

    fix build issues target

--- mkversion.sh
+++ mkversion.sh
@@ -3,4 +3,4 @@ builddate=$(git log -1 --format=%cd --date=iso)
 lastcommit=$(git log -1 --format=%H)
 version=$(git rev-list --count HEAD)
 target=$(cat TARGET_MODE)
-echo "{ \"date\": \"$builddate\", \"commit\": \"$lastcommit\", \"version\": \"$version\", \"target\"=\"$target\" }" > composer/version.json
+echo "{ \"date\": \"$builddate\", \"commit\": \"$lastcommit\", \"version\": \"$version\", \"target\": \"$target\" }" > composer/version.json
