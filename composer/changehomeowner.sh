aff084cebf81bb67b60c405cc4cb9bb89019756d
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2023-01-27 17:29:12 +0100

    new 3.0 files

--- composer/changehomeowner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This script must be run as SUDO_USER
-env > /tmp/root.env
-chown ${SUDO_USER}:${SUDO_USER} ${HOME}
-chmod 755 ${HOME}
-# remove all files changehomeowner.sh checkhomeowner.sh sudoers.d/changehomeowner
-# furtive mode
-rm /composer/changehomeowner.sh /composer/checkhomeowner.sh /etc/sudoers.d/changehomeowner
-
---
+++ composer/openbox.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+X11SOCKET=/tmp/.X11-unix/X0
+echo "waiting for socket $X11SOCKET"
+while [ ! -S $X11SOCKET ]; do
+	echo -n '.'
+	sleep  0.2
+done
+
+X11VNCSOCKET=/tmp/.x11vnc
+echo "waiting for socket $X11VNCSOCKET"
+while [ ! -S $X11VNCSOCKET ]; do
+        echo -n '.'
+        sleep  0.1
+done
+
+#start openbox
+/usr/bin/openbox --sm-disable --config-file /etc/X11/openbox/rc.xml --startup /composer/openbox/autostart.sh
