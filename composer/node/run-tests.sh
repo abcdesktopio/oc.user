69aeb4c84a81dd251a49a4d76e86c6d74ebad492
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2023-01-31 13:37:24 +0100

    run default kubernetes service

--- composer/node/run-tests.sh
+++ composer/node/run-tests.sh
@@ -2,16 +2,20 @@
 
 # set vars for test mode
 export DISPLAY=:0.0
-CONTAINER_IP=$(hostname -I | awk '{print $1}')
+CONTAINER_IP=$(hostname -i)
 
 cd /composer/node/spawner-service 
 CONTAINER_IP=${CONTAINER_IP} yarn test
 
-cd /composer/node/file-service 
-CONTAINER_IP=${CONTAINER_IP} yarn test
+# cd /composer/node/file-service 
+# CONTAINER_IP=${CONTAINER_IP} yarn test
 
 cd /composer/node/broadcast-service 
 CONTAINER_IP=${CONTAINER_IP} yarn test
 
-cd /composer/node/xterm.js 
-CONTAINER_IP=${CONTAINER_IP} yarn test
+if [ -f /TARGET_MODE ]; then
+	if [ $(cat /TARGET_MODE) != hardening ];
+		cd /composer/node/xterm.js 
+		CONTAINER_IP=${CONTAINER_IP} yarn test
+	fi
+fi
