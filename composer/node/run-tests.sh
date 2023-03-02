f1797b18e25c17887c3611ea167c32f0483e0fb0
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2023-03-02 16:11:41 +0100

    add trace

--- composer/node/run-tests.sh
+++ composer/node/run-tests.sh
@@ -4,12 +4,14 @@
 export DISPLAY=:0.0
 CONTAINER_IP=$(hostname -i)
 
+echo 'testing spawner-service'
 cd /composer/node/spawner-service 
 CONTAINER_IP=${CONTAINER_IP} yarn test
 
 # cd /composer/node/file-service 
 # CONTAINER_IP=${CONTAINER_IP} yarn test
 
+echo 'testing broadcast-service'
 cd /composer/node/broadcast-service 
 CONTAINER_IP=${CONTAINER_IP} yarn test
 
