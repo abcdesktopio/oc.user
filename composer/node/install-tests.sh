c48a4696074edaf59f00c14125ccf389f0bdce08
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2023-03-03 10:07:44 +0100

    add net-tools

--- composer/node/install-tests.sh
+++ composer/node/install-tests.sh
@@ -13,7 +13,7 @@ for f in passwd shadow group gshadow ; do rm -f /etc/$f && cp $ABCDESKTOP_LOCALA
 if [[ ${distrib} == "alpine" ]]; then
 	echo "install packages for $distrib"
 	apk add git gcc make g++ bash build-base alpine-sdk sudo wget python3 libx11-dev yarn
-	apk add xeyes
+	apk add xeyes net-tools
 fi
 
 if [[ ${distrib} == "ubuntu" ]]; then
@@ -23,7 +23,7 @@ if [[ ${distrib} == "ubuntu" ]]; then
 	curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | tee /usr/share/keyrings/yarnkey.gpg >/dev/nul
 	echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | tee /etc/apt/sources.list.d/yarn.list
 	apt-get update && apt-get install -y yarn
-	apt-get install -y x11-apps
+	apt-get install -y x11-apps net-tools
 fi
 
 # install yarn with npm install
@@ -46,3 +46,4 @@ cd /composer/node/xterm.js
 yarn install --production=false
 
 echo "full install yarn done"
+
