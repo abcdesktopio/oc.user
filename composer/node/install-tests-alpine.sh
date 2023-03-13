4c0adcee6523cd0b73222cef916b6ce2f6200220
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2023-03-13 15:37:06 +0100

    remove file

--- composer/node/install-tests-alpine.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# The default install use yarn install
-# yarn install --production=true
-# this script install missing package with --production=false option
-# yarn install --production=false
-
-apk add git gcc make g++ bash build-base alpine-sdk sudo wget python3 libx11-dev yarn
-
