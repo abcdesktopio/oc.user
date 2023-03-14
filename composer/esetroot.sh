af6836a39de20369527e0c306b4cba3b5afc39e3
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2023-03-14 09:35:16 +0100

    add Esetroot support

--- composer/esetroot.sh
+++ composer/esetroot.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 bgColor=$1 
 imgName=$2
-killall /usr/bin/feh
-/usr/bin/feh --no-fehbg --bg-center --borderless --image-bg "${bgColor}" "${imgName}"
+if [ -x /usr/bin/Esetroot ]; then
+      	/usr/bin/Esetroot -bg "${bgColor}" -center -fit  "${imgName}"
+else      
+     	killall /usr/bin/feh
+	/usr/bin/feh --no-fehbg --bg-center --borderless --image-bg "${bgColor}" "${imgName}"
+fi
