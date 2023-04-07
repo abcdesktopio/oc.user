a4451763382cd521d63a061e04d72794fcdc7bdc
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2023-04-07 18:21:13 +0200

    fix path

--- composer/unaccount.sh
+++ composer/unaccount.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-rm /etc/passwd /etc/group /etc/shadow /etc/gshadow
-cp /var/secrets/abcdesktop/localaccount/passwd .
-cp /var/secrets/abcdesktop/localaccount/gshadow .
-cp /var/secrets/abcdesktop/localaccount/shadow .
-cp /var/secrets/abcdesktop/localaccount/group .
+for f in passwd shadow group gshadow ; do  
+	rm -f /etc/$f; 
+	cp /var/secrets/abcdesktop/localaccount/$f /etc/$f; 
+done
