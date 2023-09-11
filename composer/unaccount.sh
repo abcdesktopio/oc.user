d1698399f71355135d05b37418ba274d79d8a791
Alexandre DEVELY <12896316+alexandredevely@users.noreply.github.com>
2023-09-11 10:21:05 +0200

    Update unaccount.sh

--- composer/unaccount.sh
+++ composer/unaccount.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+if [ -z "$LOCALACCOUNT_PATH" ]; then
+	# default path in 3.1 release
+	LOCALACCOUNT_PATH='/etc/localaccount'
+fi 
+
 for f in passwd shadow group gshadow ; do  
 	rm -f /etc/$f; 
-	cp /var/secrets/abcdesktop/localaccount/$f /etc/$f; 
+ 	echo "repplace file /etc/$f by $LOCALACCOUNT_PATH/$f";
+	cp "$LOCALACCOUNT_PATH/$f" "/etc/$f"; 
 done
