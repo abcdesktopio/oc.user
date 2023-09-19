3bc09f14ce741d6891d1abb6d158ef62947c80a5
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2023-09-19 12:05:40 +0200

    add safe normalize path

--- composer/node/spawner-service/lib_spawner/utils.js
+++ composer/node/spawner-service/lib_spawner/utils.js
@@ -12,19 +12,48 @@
 */
 
 const fs = require('fs');
+const path = require('path');
+
+/**
+ *
+ * @param {string} key
+ * @param {string} path
+ */
+function safeNormalizePath( key, localDirectory )
+{
+  // default return code
+  // location direcotry of .store file 
+  const store_directory = path.join(process.env.HOME, localDirectory);
+  // location directory of resquested file
+  const requestedfile = path.join(store_directory,key);
+  // normalize the requested file
+  const normalize_requestedfile = path.normalize( requestedfile );
+  var requested_directory = path.dirname(normalize_requestedfile);
+  // if the reqested directory is not $HOME/.store
+  // return false
+  if (requested_directory.startsWith(store_directory))
+        return normalize_requestedfile;
+  else
+        return undefined;
+}
+
 
 /**
  *
  * @param {string} key
  */
 async function get(key) {
-  const fullpath = `${process.env.HOME}/.store/${key}`;
-  console.log(`get : ${fullpath}`);
-
   const ret = { code: 404, data: 'Not found' };
+  const normalize_requestedfile = safeNormalizePath( key, '.store');
+  if (!normalize_requestedfile) {
+        // this can be an error or an attack
+        console.error( `normalize_requestedfile has failed, path not in ${process.env.HOME}/.store requested ${key}` );
+        return ret;
+  }
+
   try {
-    await fs.promises.access(fullpath, fs.constants.R_OK);
-    ret.data = await fs.promises.readFile(fullpath, 'utf8');
+    await fs.promises.access(normalize_requestedfile, fs.constants.R_OK);
+    ret.data = await fs.promises.readFile(normalize_requestedfile, 'utf8');
     ret.code = 200;
   } catch(e) {
     console.error(e);
@@ -39,9 +68,13 @@ async function get(key) {
  * @param {string} value
  */
 async function set(key, value) {
-  const fullpath = `${process.env.HOME}/.store/${key}`;
-  console.log(`set : ${fullpath}`);
-  await fs.promises.writeFile(fullpath, value);
+  const normalize_requestedfile = safeNormalizePath( key, '.store');
+  if (normalize_requestedfile) {
+  	console.log(`set : ${normalize_requestedfile}`);
+  	await fs.promises.writeFile(fullpath, value);
+  }
+  else
+    console.error( `normalize_requestedfile has failed, path not in ${process.env.HOME}/.store requested ${key}` );
 }
 
 /**
