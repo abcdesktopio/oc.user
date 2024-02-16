f463ea5d5da19c3a61cdaea4da959b26e6ac4959
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2024-02-16 12:17:24 +0100

    Release

--- composer/node/spawner-service/lib_spawner/colorflow/build/Release/.deps/Release/colorflow.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/colorflow.node := ln -f "Release/obj.target/colorflow.node" "Release/colorflow.node" 2>/dev/null || (rm -rf "Release/colorflow.node" && cp -af "Release/obj.target/colorflow.node" "Release/colorflow.node")
--- composer/node/spawner-service/lib_spawner/colorflow/build/Release/.deps/Release/obj.target/colorflow.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/obj.target/colorflow.node := g++ -o Release/obj.target/colorflow.node -shared -pthread -rdynamic -m64  -Wl,-soname=colorflow.node -Wl,--start-group Release/obj.target/colorflow/native/libnsbmp.o Release/obj.target/colorflow/native/libcolorflow.o Release/obj.target/colorflow/native/colorflow.o -Wl,--end-group -lpng -ljpeg
