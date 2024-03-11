e593490ba9a842aec094851cc51798bf25bd2e92
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2024-03-11 16:22:00 +0100

    new account file

---
+++ composer/doaccount.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+for f in passwd shadow group gshadow ; do if [ -f /etc/$f ] ; then  cp /etc/$f $ABCDESKTOP_LOCALACCOUNT_DIR ; rm -f /etc/$f; ln -s $ABCDESKTOP_LOCALACCOUNT_DIR/$f /etc/$f; fi; done
