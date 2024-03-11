dd05e2ef0d968ff73ff0e3418182c115c8cb2217
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2024-03-11 16:19:14 +0100

    use ABCDESKTOP_LOCALACCOUNT_DIR

--- composer/unaccount.sh
+++ composer/unaccount.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-if [ -z "$LOCALACCOUNT_PATH" ]; then
-	# default path in 3.1 release
-	LOCALACCOUNT_PATH='/etc/localaccount'
-fi 
 
 for f in passwd shadow group gshadow ; do  
 	rm -f /etc/$f; 
- 	echo "repplace file /etc/$f by $LOCALACCOUNT_PATH/$f";
-	cp "$LOCALACCOUNT_PATH/$f" "/etc/$f"; 
+ 	echo "replace file /etc/$f by $ABCDESKTOP_LOCALACCOUNT_DIR/$f";
+	cp "$ABCDESKTOP_LOCALACCOUNT_DIR/$f" "/etc/$f"; 
 done
