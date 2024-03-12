a2cec1b6d22e587d839efad7601a624210e7e0b6
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2024-03-12 11:47:18 +0100

    add ABCDESKTOP_LOCALACCOUNT_DIR default value

--- composer/doaccount.sh
+++ composer/doaccount.sh
@@ -1,2 +1,3 @@
 #/bin/bash
+ABCDESKTOP_LOCALACCOUNT_DIR=${ABCDESKTOP_LOCALACCOUNT_DIR:-'/etc/localaccount'}
 for f in passwd shadow group gshadow ; do if [ -f /etc/$f ] ; then  cp /etc/$f $ABCDESKTOP_LOCALACCOUNT_DIR ; rm -f /etc/$f; ln -s $ABCDESKTOP_LOCALACCOUNT_DIR/$f /etc/$f; fi; done
--- composer/unaccount.sh
+++ composer/unaccount.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+ABCDESKTOP_LOCALACCOUNT_DIR=${ABCDESKTOP_LOCALACCOUNT_DIR:-'/etc/localaccount'}
 
 for f in passwd shadow group gshadow ; do  
 	rm -f /etc/$f; 
