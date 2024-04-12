74fbcd1cba75c0304d91a3591c975d6dd0414680
Alexandre DEVELY <12896316+alexandredevely@users.noreply.github.com>
2024-04-12 13:58:37 +0200

    Update xserver-chooser.sh

--- composer/xserver-chooser.sh
+++ composer/xserver-chooser.sh
@@ -6,10 +6,10 @@ if [ -d /proc/driver/nvidia ]; then
        if [ -x /usr/bin/nvidia-smi ]; then
 	       	echo command line /usr/bin/nvidia-smi found
        		/usr/bin/nvidia-smi --query-gpu=gpu_name,gpu_bus_id,vbios_version --format=csv
-        	if [ $? -eq 0 ]; then
-			/composer/xserver-nvidia.sh
-			exit 0
-		fi
+        	# if [ $? -eq 0 ]; then
+		#	/composer/xserver-nvidia.sh
+		#	exit 0
+		# fi
 	fi
 fi
 
