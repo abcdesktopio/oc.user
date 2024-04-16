91c6840523552e10a9825278de50f7a5ee542f1f
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2024-04-16 11:13:01 +0200

    add ABCDESKTOP_USE_X11_NVIDIA

--- composer/xserver-chooser.sh
+++ composer/xserver-chooser.sh
@@ -6,10 +6,14 @@ if [ -d /proc/driver/nvidia ]; then
        if [ -x /usr/bin/nvidia-smi ]; then
 	       	echo command line /usr/bin/nvidia-smi found
        		/usr/bin/nvidia-smi --query-gpu=gpu_name,gpu_bus_id,vbios_version --format=csv
-        	# if [ $? -eq 0 ]; then
-		#	/composer/xserver-nvidia.sh
-		#	exit 0
-		# fi
+        	if [ $? -eq 0 ]; then
+			echo "ABCDESKTOP_USE_X11_NVIDIA=${ABCDESKTOP_USE_X11_NVIDIA}"
+			if [ ! -z ${ABCDESKTOP_USE_X11_NVIDIA} ]; then 
+				echo "${ABCDESKTOP_USE_X11_NVIDIA} is set"
+				/composer/xserver-nvidia.sh
+			        exit 0
+			fi
+		fi
 	fi
 fi
 
