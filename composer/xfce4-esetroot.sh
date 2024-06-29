008bd78ae387313af6bcbd985afb7fc9256c204c
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2024-06-29 14:33:00 +0200

    fix Property does not exist on channel for self test

--- composer/xfce4-esetroot.sh
+++ composer/xfce4-esetroot.sh
@@ -10,15 +10,16 @@ ESETROOT_LOGFILE=$ABCDESKTOP_LOG_DIR/esetroot.log
 echo "bgColor=$RrbgColor $GrbgColor $BrbgColor" &>> $ESETROOT_LOGFILE
 echo "imgName=$imgName" &>> $ESETROOT_LOGFILE
 
+/usr/bin/xfconf-query --create -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/rgba1
 /usr/bin/xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/rgba1 -t double -t double -t double -t double -s $RrbgColor -s $GrbgColor -s $BrbgColor -s 1
 
 
 if [ ! -z "$imgName" ]; then
   # set image style to centered 
-  /usr/bin/xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/image-style -s 1
+  /usr/bin/xfconf-query --create -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/image-style -s 1
   # set image 
-  /usr/bin/xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/last-image -s "$imgName" --create
+  /usr/bin/xfconf-query --create -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/last-image -s "$imgName"
 else
   # set solid color
-  /usr/bin/xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/color-style -s 0
+  /usr/bin/xfconf-query --create -c xfce4-desktop -p /backdrop/screen0/monitorVNC-0/workspace0/color-style -s 0
 fi
