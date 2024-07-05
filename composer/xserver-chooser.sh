3a06142a642bb53e2c30a9bca3c2560167dbeeae
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2024-07-05 18:16:12 +0200

    change command

--- composer/xserver-chooser.sh
+++ composer/xserver-chooser.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
+# check if /proc/driver/nvidia exists
 if [ -d /proc/driver/nvidia ]; then
        echo /proc/driver/nvidia exists
        # suppose there is an gpu 
        if [ -x /usr/bin/nvidia-smi ]; then
 	       	echo command line /usr/bin/nvidia-smi found
-       		/usr/bin/nvidia-smi --query-gpu=gpu_name,gpu_bus_id,vbios_version --format=csv
-        	if [ $? -eq 0 ]; then
+       		if /usr/bin/nvidia-smi --query-gpu=gpu_name,gpu_bus_id,vbios_version --format=csv; then
 			echo "ABCDESKTOP_USE_X11_NVIDIA=${ABCDESKTOP_USE_X11_NVIDIA}"
 			if [ ! -z ${ABCDESKTOP_USE_X11_NVIDIA} ]; then 
 				echo "${ABCDESKTOP_USE_X11_NVIDIA} is set"
 				exec /composer/xserver-nvidia.sh
-			        exit 0
 			fi
 		fi
 	fi
