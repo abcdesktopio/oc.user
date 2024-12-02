3346a375f4fd067eafa0b6493892a21f8e1d7db6
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2024-12-02 18:09:45 +0100

    change yarn install

--- composer/node/install-tests.sh
+++ composer/node/install-tests.sh
@@ -5,30 +5,27 @@
 # this script install missing package with --production=false option
 # yarn install --production=false
 
-distrib=$(awk '/^ID=/' /etc/*-release | sed 's/ID=//' | tr '[:upper:]' '[:lower:]')
-echo "distrib is $distrib"
+source /etc/os-release 
+echo "distrib $ID $VERSION"
 
 for f in passwd shadow group gshadow ; do rm -f /etc/$f && cp $ABCDESKTOP_LOCALACCOUNT_DIR/$f /etc/$f; done
 
-if [[ ${distrib} == "alpine" ]]; then
-	echo "install packages for $distrib"
+if [[ ${ID} == "alpine" ]]; then
+	echo "install packages for $ID"
 	apk add git gcc make g++ bash build-base alpine-sdk sudo wget python3 libx11-dev yarn
 	apk add xeyes net-tools
 fi
 
-if [[ ${distrib} == "ubuntu" ]]; then
-	echo "install packages for $distrib"
-	echo "apt-get update"
+if [[ ${ID} == "ubuntu" ]]; then
+	echo "install packages for $ID"
 	apt-get update
 	echo "apt-get install -y curl libxmu-dev gcc g++ make libx11-dev libxmu-dev git libimlib2-dev libpng-dev"
 	apt-get install -y curl libxmu-dev gcc g++ make libx11-dev libxmu-dev git libimlib2-dev libpng-dev
-	curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | tee /usr/share/keyrings/yarnkey.gpg >/dev/nul
-	echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | tee /etc/apt/sources.list.d/yarn.list
-	apt-get update && apt-get install -y yarn
 	apt-get install -y x11-apps net-tools
 fi
 
 # install yarn with npm install
+echo "install yarn using npm"
 npm install -g yarn
 # install full options without production
 
