048a47b0b6300c7950a39d4913af092a8c2e6c5b
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2025-05-27 16:03:23 +0200

    fix shadow

--- composer/unaccount.sh
+++ composer/unaccount.sh
@@ -2,8 +2,14 @@
 
 ABCDESKTOP_LOCALACCOUNT_DIR=${ABCDESKTOP_LOCALACCOUNT_DIR:-'/etc/localaccount'}
 
-for f in passwd shadow group gshadow ; do  
-	rm -f /etc/$f; 
+for f in passwd group ; do  
+	rm -f /etc/${f}; 
  	echo "replace file /etc/$f by $ABCDESKTOP_LOCALACCOUNT_DIR/$f";
-	cp "$ABCDESKTOP_LOCALACCOUNT_DIR/$f" "/etc/$f"; 
+	cp "${ABCDESKTOP_LOCALACCOUNT_DIR}/${f}" "/etc/${f}"; 
+done
+
+for f in shadow gshadow ; do
+        rm -f /etc/${f};
+        echo "replace file /etc/$f by $ABCDESKTOP_LOCALACCOUNT_DIR/$f";
+        cp "${ABCDESKTOP_LOCALACCOUNT_DIR}.shadow/${f}" "/etc/${f}";
 done
