162e54151e4c2a796ff73c4c89af8c6d29045c7f
Alexandre DEVELY <12896316+alexandredevely@users.noreply.github.com>
2025-07-10 14:08:37 +0200

    fix https://github.com/abcdesktopio/oc.user/issues/57

--- composer/connectcount.sh
+++ composer/connectcount.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-COUNT=$(netstat -nt | grep 'ESTABLISHED' | grep ':6081 ' | wc -l)
+# fixed by richardmeilinger https://github.com/abcdesktopio/oc.user/issues/57
+COUNT=$(LC_ALL=C netstat -nt | grep 'ESTABLISHED' | grep ':6081 ' | wc -l)
 echo $COUNT
 
 
