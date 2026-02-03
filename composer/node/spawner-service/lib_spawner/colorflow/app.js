5812afc440c7d56e6a44ce45608c2032831efd83
Matt307082 <matteobeghelli2003@gmail.com>
2026-02-03 14:08:28 +0100

    clone colorflow dynamically in Dockerfile

--- composer/node/spawner-service/lib_spawner/colorflow/app.js
+++
@@ -1,8 +0,0 @@
-const c = require("./colorflow");
-
-console.log(c("images/red.bmp"))
-console.log(c("images/green.bmp"))
-console.log(c("images/blue.bmp"))
-console.log(c("images/black.bmp"))
-console.log(c("images/white.bmp"))
-console.log(c("images/lake.bmp"))
