e897cba214d6e19e8e0ecfd19ce9ab524f314a28
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2026-03-16 15:10:33 +0100

    new file

---
+++ composer/set-wallpaper.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# 
+# this script add line "var launchers=\"${LAUNCHERS}\"; to add-to-quicklaunch.js and runs the javascript
+# javascript add-to-quicklaunch update the launcher ison for plasmashell without restarting it
+#
+#
+IMAGE=$1 
+
+export ABCDESKTOP_RUN_DIR=${ABCDESKTOP_RUN_DIR:-'/var/run/desktop'}
+export ABCDESKTOP_LOG_DIR=${ABCDESKTOP_LOG_DIR:-'var/log/desktop'}
+
+# find qdbus /usr/lib/qt6/bin/qdbus is not in default PATH
+qdbus=/usr/bin/qdbus
+if [[ -x /usr/lib/qt6/bin/qdbus ]]; then
+        qdbus=/usr/lib/qt6/bin/qdbus
+fi
+echo ${qdbus}
+
+# wait for plasmashell running
+# else /usr/bin/qdbus  org.kde.plasmashell org.kde.PlasmaShell.evaluateScript failed
+until pids=$($qdbus org.kde.plasmashell 2>/dev/null)
+do
+    sleep 1
+done
+
+${qdbus} org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript "var image=\"${IMAGE}\";  $(cat /composer/set-wallpaper.js)"
